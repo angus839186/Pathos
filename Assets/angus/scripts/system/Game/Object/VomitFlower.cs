87cee4a3a549d3e9662416bd08f23abb7d21888b
angus839186 <angus839186@gmail.com>
2025-04-02 04:14:52 +0800

    音樂花 做到一半

---
+++ Assets/angus/scripts/system/Game/Object/VomitFlower.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VomitFlower : MonoBehaviour
+{
+    private string musicPassword;
+
+    private int currentIndex;
+    void Start()
+    {
+        
+    }
+
+    void CheckMusicOrder()
+    {
+
+    }
+}
