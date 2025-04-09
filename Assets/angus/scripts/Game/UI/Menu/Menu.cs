7494e758a9274d02eb82084a13d318de880795c3
angus839186 <angus839186@gmail.com>
2025-04-10 00:02:54 +0800

    Event修正

--- Assets/angus/scripts/Game/UI/Menu/Menu.cs
+++ Assets/angus/scripts/Game/UI/Menu/Menu.cs
@@ -20,6 +20,7 @@ public class Menu : MonoBehaviour
     }
     public void Transition(CanvasGroup newCanvas)
     {
+        Debug.Log(currentCanva);
         StartCoroutine(TransitionCoroutine(newCanvas));
     }
 
