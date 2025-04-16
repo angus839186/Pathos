f35b9b6bcafa9ef4d53e7746db825588d8f47ea9
angus839186 <angus839186@gmail.com>
2025-04-17 00:13:22 +0800

    樂手更新

--- Assets/angus/scripts/Game/Object/NPC/Musician.cs
+++ Assets/angus/scripts/Game/Object/NPC/Musician.cs
@@ -9,6 +9,16 @@ public class Musician : InteractableNPC, IDataPersistence
     public DialogueObject normalDialogue;
     public DialogueObject TakeMusicScoreDialogue;
 
+    public AudioSource MusicianAudio;
+
+    public Animator anime;
+
+    void Start()
+    {
+        anime = GetComponent<Animator>();
+        MusicianAudio = GetComponent<AudioSource>();
+    }
+
 
     public override DialogueObject GetDialogue()
     {
@@ -37,5 +47,23 @@ public class Musician : InteractableNPC, IDataPersistence
     public void SaveData(ref GameData data)
     {
         data.MusicianGotMusicScore = gotMusicScore;
+    }
+    public void PlaySong()
+    {
+        StartCoroutine(Singing());
+    }
+
+    public IEnumerator Singing()
+    {
+        MusicianAudio.Play();
+        anime.SetBool("singing", true);
+        BoxCollider2D collider = GetComponent<BoxCollider2D>();
+        collider.enabled = false;
+        while (MusicianAudio.isPlaying)
+        {
+            yield return null;
+        }
+        collider.enabled = true;
+        anime.SetBool("singing", false);
     }
 }
